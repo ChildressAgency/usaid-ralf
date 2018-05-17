0af6cda9ed401875dea0fa9385df4d3c1be86f7b
childressdev <jcampbell@childressagency.com>
2018-05-17 11:56:39 -0400

    styled and checkboxes work

--- js/usaid-ralf-scripts.js
+++ js/usaid-ralf-scripts.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function($){
-  $('.factor-grid').masonry({
+/*$('.factor-grid').masonry({
     itemSelector: '.grid-item',
-    gutter: 5
-  });
+    gutter: 5,
+    //horizontalOrder:true,
+    fitWidth:true
+  });*/
+
+  $('.factor-grid').on('change', 'input[type="checkbox"]', function(){
+    if(this.checked){
+      $(this).parent().addClass('active');
+    }
+    else{
+      $(this).parent().removeClass('active');
+    }
+  })
 });
