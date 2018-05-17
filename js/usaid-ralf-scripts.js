810f5214c10c7ce8d4c592d20d5602c3aa31268e
childressdev <jcampbell@childressagency.com>
2018-05-17 12:08:28 -0400

    removed masonry code

--- js/usaid-ralf-scripts.js
+++ js/usaid-ralf-scripts.js
@@ -1,11 +1,4 @@
 jQuery(document).ready(function($){
-/*$('.factor-grid').masonry({
-    itemSelector: '.grid-item',
-    gutter: 5,
-    //horizontalOrder:true,
-    fitWidth:true
-  });*/
-
   $('.factor-grid').on('change', 'input[type="checkbox"]', function(){
     if(this.checked){
       $(this).parent().addClass('active');
