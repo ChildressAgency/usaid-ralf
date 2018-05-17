7b1ca2c99534764e9b8adc4bf9c79863a856a724
childressdev <jcampbell@childressagency.com>
2018-05-17 17:40:43 -0400

    styled and sidebar affix working

--- js/usaid-ralf-scripts.js
+++ js/usaid-ralf-scripts.js
@@ -6,5 +6,28 @@ jQuery(document).ready(function($){
     else{
       $(this).parent().removeClass('active');
     }
-  })
+  });
+
+  var headerHeight = $('#header-nav').outerHeight(true);
+  var footerHeight = $('#footer').outerHeight(true);
+  var $resultsSidebar = $('.results-sidebar');
+
+  $resultsSidebar.on('affix.bs.affix', function(){
+    $(this).css({'top': 25, 'margin-top': 0});
+  });
+  $resultsSidebar.on('affix-top.bs.affix', function(){
+    $(this).css({'margin-top':80});
+  });
+
+  $resultsSidebar.affix({
+    offset:{
+      //top:headerHeight,
+      top: function(){
+        return headerHeight + 55;
+      },
+      bottom:function(){
+        return footerHeight + 40;
+      }
+    }
+  });
 });
