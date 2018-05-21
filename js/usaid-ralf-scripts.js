7a763582fb07094616217ed82c1fd900aeab4806
childressdev <jcampbell@childressagency.com>
2018-05-21 17:04:09 -0400

    report button code

--- js/usaid-ralf-scripts.js
+++ js/usaid-ralf-scripts.js
@@ -30,4 +30,57 @@ jQuery(document).ready(function($){
       }
     }
   });
+
+  //add to report
+  var reportIds = Cookies.get('report_ids');
+  var reportIdArr;
+  //var report_button = '<a href="#" id="saveToReport" class="save-to-report">Save To Report</a>';
+  $('.report-button').each(function () {
+    $(this).html('<a href="#" id="saveToReport" class="btn-main btn-report">Save To Report</a>');
+  });
+
+  if (reportIds) {
+    var reportIdArr = reportIds.split(',');
+
+    if (reportIdArr.indexOf(articleId) > -1) {
+      //report_button = '<a href="#" id="removeFromReport" class="save-to-report">Remove From Report</a>';
+      $('.report-button').each(function () {
+        $(this).html('<a href="#" id="removeFromReport" class="btn-main btn-report">Remove From Report</a>');
+      });
+    }
+  }
+
+  $('.report-button').on('click', '#saveToReport', function (e) {
+    e.preventDefault();
+    if (reportIdArr) {
+      if (reportIdArr.indexOf(articleId) < 0) {
+        reportIdArr.push(articleId);
+      }
+
+      reportIds = reportIdArr.toString();
+    }
+    else {
+      reportIds = articleId;
+    }
+
+    Cookies.set('report_ids', reportIds, { expires: 30 });
+    $('.report-button').each(function () {
+      $(this).html('<span><em>Added to report</em></span><a href="#" id="removeFromReport" class="btn-main btn-report">Remove From Report</a>');
+    });
+  });
+
+  $('.report-button').on('click', '#removeFromReport', function (e) {
+    e.preventDefault();
+
+    var idIndex = reportIdArr.indexOf(articleId);
+    if (idIndex > -1) {
+      reportIdArr.splice(idIndex, 1);
+    }
+
+    reportIds = reportIdArr.toString();
+    Cookies.set('report_ids', reportIds, { expires: 30 });
+    $('.report-button').each(function () {
+      $(this).html('<span><em>Removed from report</em></span><a href="#" id="saveToReport" class="btn-main btn-report">Save To Report</a>');
+    });
+  });  
 });
