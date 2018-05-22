fc5ca2fbaa7f06d0d796c5006fcd8975d768ec4c
childressdev <jcampbell@childressagency.com>
2018-05-22 11:08:05 -0400

    added

---
+++ page-view-report.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<div class="page-content">
+  <div class="container">
+    <main class="results-list">
+      <header class="page-header">
+        <h1>Report of Activities and Associated Impacts</h1>
+      </header>
+        <?php
+          if(isset($_COOKIE['report_ids'])){
+            $report_ids_cookie = $_COOKIE['report_ids'];
+
+            $report_ids = explode(',', $report_ids_cookie);
+
+            $activities_ids = array_map(
+              function($value){ return (int)$value; },
+              $report_ids
+            );
+
+            $activities_report = new WP_Query(array(
+              'post_type' => 'activities',
+              'posts_per_page' => -1,
+              'post__in' => $activities_ids
+            ));
+
+            if($activities_report->have_posts()): while($activities_report->have_posts()): $activities_report->the_post();
+
+              get_template_part('partials/report', 'loop');
+
+            endwhile; endif; wp_reset_postdata(); 
+          }
+          else{
+            echo '<p>You haven\'t saved any Activities to report.</p>';
+          }
+        ?>
+    </main>
+  </div>
+</div>
+<?php get_footer(); ?>
---
+++ partials/report-loop.php
@@ -0,0 +1,51 @@
+<article>
+  <header class="result-header">
+    <h1><?php the_title(); ?></h1>
+  </header>
+  <section class="result-content">
+    <div class="activity-description">
+      <p><?php the_content(); ?></p>
+    </div>
+    <?php if(get_field('conditions')): ?>
+      <div class="activity-conditions">
+        <h2>CONDITIONS</h2>
+        <?php the_field('conditions'); ?>
+      </div>
+    <?php endif; ?>
+  </section>
+
+  <?php
+    $impact_ids = get_field('related_impacts', false, false);
+    if(!empty($impact_ids)):
+      $impacts_by_sector = usaidralf_get_impacts_by_sector($impact_ids); ?>
+
+      <section class="impact-by-sector">
+        <h2>IMPACT BY SECTOR</h2>
+        <div class="panel-group" role="tablist">
+          <?php
+            foreach($impacts_by_sector as $sector):
+              $acf_sector_id = 'sector_' . $sector->sector_id;
+              foreach($sector->impacts as $impact): ?>
+
+                <div class="panel panel-default">
+                  <div class="panel-heading" role="tab">
+                    <h3 class="panel-title"><a href="#"><?php echo $impact->name; ?></a></h3>
+                  </div>
+                  <div class="panel-body">
+                    <?php echo $impact->impact_description; ?>
+                  </div>
+                </div>
+
+            <?php endforeach; ?>
+          <?php endforeach; ?>
+        </div>
+      </section>
+  <?php endif; ?>
+
+  <?php $article_id = get_the_ID(); ?>
+  <script>var articleId = "<?php echo $article_id; ?>";</script>
+
+  <div class="report-button hidden-print">
+
+  </div>
+</article>
