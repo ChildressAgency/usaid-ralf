a12f4adb61d383efbdabe7947295d42676145d53
childressdev <jcampbell@childressagency.com>
2018-05-22 15:29:21 -0400

    hide impacts with 0 activities

--- partials/impact-archive-loop.php
+++ partials/impact-archive-loop.php
@@ -1,28 +1,33 @@
-<div class="media">
-  <div class="media-left">
-    <span class="result-type-icon impact"></span>
-  </div>
-  <div class="media-body">
-    <h3 class="media-heading"><?php the_title(); ?></h3>
-    <?php
-      $impact_id = get_the_ID();
-      $sectors = get_the_terms($impact_id, 'sectors');
-      foreach($sectors as $sector): ?>
-        <?php $acf_sector_id = 'sectors_' . $sector->term_id; ?>
-        <div class="row">
-          <div class="col-sm-4 col-md-3">
-            <div class="sector-icon">
-              <img src="<?php the_field('sector_icon', $acf_sector_id); ?>" class="img-circle img-responsive" alt="<?php echo $sector->name; ?> Sector" style="background-color:<?php the_field('sector_color', $acf_sector_id); ?>;" />
-            </div>
-          </div>
-          <div class="col-sm-8 col-md-9">
-            <div class="number-activities">
-              <?php $num_activities = usaidralf_get_related_activities($impact_id); ?>
-              <a href="<?php the_permalink(); ?>"><span><?php echo $num_activities->post_count; ?></span>Activities</a>
-            </div>
-          </div>
-        </div>
-      
-    <?php endforeach; ?>
-  </div>
-</div>
+<?php
+  $impact_id = get_the_ID();
+  $num_activities = usaidralf_get_related_activities($impact_id);
+  if($num_activities->post_count > 0): ?>
+  
+    <div class="media">
+      <div class="media-left">
+        <span class="result-type-icon impact"></span>
+      </div>
+      <div class="media-body">
+        <h3 class="media-heading"><?php the_title(); ?></h3>
+        <?php
+          $sectors = get_the_terms($impact_id, 'sectors');
+          foreach($sectors as $sector): ?>
+            <?php $acf_sector_id = 'sectors_' . $sector->term_id; ?> 
+
+                <div class="row">
+                  <div class="col-sm-4 col-md-3">
+                    <div class="sector-icon">
+                      <img src="<?php the_field('sector_icon', $acf_sector_id); ?>" class="img-circle img-responsive" alt="<?php echo $sector->name; ?> Sector" style="background-color:<?php the_field('sector_color', $acf_sector_id); ?>;" />
+                    </div>
+                  </div>
+                  <div class="col-sm-8 col-md-9">
+                    <div class="number-activities">
+                      <a href="<?php the_permalink(); ?>"><span><?php echo $num_activities->post_count; ?></span>Activities</a>
+                    </div>
+                  </div>
+                </div>
+          
+        <?php endforeach; ?>
+      </div>
+    </div>
+<?php endif; ?>
