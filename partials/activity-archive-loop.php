dc44c44e302aeedc295b977e2fe9d6328d10bfaa
childressdev <jcampbell@childressagency.com>
2018-05-30 16:31:30 -0400

    fixed for sub-sectors

--- partials/activity-archive-loop.php
+++ partials/activity-archive-loop.php
@@ -9,13 +9,41 @@
       if(!empty($impact_ids)):
         $impacts_by_sector = usaidralf_get_impacts_by_sector($impact_ids);
 
-        foreach($impacts_by_sector as $sector): ?>
+        foreach($impacts_by_sector as $sector):
+          $acf_sector_id = '';
+          $acf_sub_sector_id = '';
+          $sector_name = '';
+          $sector_url = '';
+
+          $sector_term = get_term($sector['sector_id'], 'sectors');
+
+          if($sector_term->parent == 0){ //top level sector
+            $acf_sector_id = 'sectors_' . $sector_term->term_id;
+            $sector_name = $sector_term->name;
+            $sector_url = esc_url(get_term_link($sector_term->term_id), 'sectors');
+          }
+          else{ //sub-sector
+            $acf_sector_id = 'sectors_' . $sector_term->parent;
+            $acf_sub_sector_id = 'sectors_' . $sector_term->term_id;
+            
+            $sector_parent = get_term($sector_term->parent, 'sectors');
+            $sector_name = $sector_parent->name;
+            $sector_url = esc_url(get_term_link($sector_parent->term_id), 'sectors');
+          }
+        ?>
+          
           <div class="row">
             <div class="col-sm-4 col-md-3">
-              <a href="<?php echo esc_url(get_term_link($sector->term_id), 'sectors'); ?>" class="sector-icon" data-toggle="tooltip" data-placement="top" title="<?php echo $sector['sector_name']; ?>">
-                <?php $acf_sector_id = 'sectors_' . $sector['sector_id']; ?>
-                <img src="<?php the_field('sector_icon', $acf_sector_id); ?>" class="img-circle img-responsive" alt="<?php echo $sector['sector_name']; ?>" style="background-color:<?php the_field('sector_color', $acf_sector_id); ?>;" />
-              </a>
+              <div class="sector-icon">
+                <a href="<?php echo $sector_url; ?>" class="parent-sector" data-toggle="tooltip" data-placement="top" title="<?php echo $sector_name; ?>">
+                  <img src="<?php the_field('sector_icon', $acf_sector_id); ?>" class="img-circle img-responsive" alt="<?php echo $sector_name; ?> Sector" style="background-color:<?php the_field('sector_color', $acf_sector_id); ?>;" />
+                </a>
+                <?php if($sector_term->parent > 0): ?>
+                  <a href="<?php echo esc_url(get_term_link($sector_term->term_id, 'sectors')); ?>" class="sub-sector" data-toggle="tooltip" data-placement="top" title="<?php echo $sector_term->name; ?>">
+                    <img src="<?php the_field('sector_icon', $acf_sub_sector_id); ?>" class="img-circle img-responsive" alt="<?php echo $sector_term->name; ?> Sector" style="background-color:<?php the_field('sector_color', $acf_sub_sector_id); ?>" />
+                  </a>
+                <?php endif; ?>
+              </div>
             </div>
             <div class="col-sm-8 col-md-9">
               <div class="factor-grid">
