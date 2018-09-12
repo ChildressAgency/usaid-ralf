9bf3c38cf1c47d1434089ac67e3de77ca656c091
childressdev <jcampbell@childressagency.com>
2018-09-12 12:14:34 -0400

    moved

--- ralf_reports/ralf_reports.php
+++ ralf_reports/ralf_reports.php
@@ -22,7 +22,7 @@ class ralf_report{
   }
 
   function ralf_report(){
-    include 'view-report.php';
+    include('view-report.php');
   }
 
   function scripts(){
@@ -62,25 +62,31 @@ class ralf_report{
       wp_send_json_error();
     }
 
-    include('rtfgenerator/class_rtf.php');
-    include('generate_rtf_report.php');
+    include('class_rtf.php');
+    //include('generate_rtf_report.php');
 
-    $rtf = new rtf('rtfgenerator/rtf_config.php');
+    $rtf_report = $this->get_report($report_ids_array);
+
+    $rtf = new rtf('rtf_config.php');
     $rtf->setPaperSize(5);
     $rtf->setPaperOrientation(1);
     $rtf->setDefaultFontFace(0);
     $rtf->setDefaultFontSize(24);
-    $rtf->setAuthor("noginn");
+    $rtf->setAuthor("jcampbell");
     $rtf->setOperator("jcampbell@childressagency.com");
     $rtf->setTitle("RALF Impact Report");
     $rtf->addColour("#000000");
     //$rtf->addText($_POST['text']);
-    $rtf->addText($this->get_report($report_ids_array));
+    $rtf->addText($rtf_report);
     $rtf->getDocument();
+
+//$rtf = $this->get_report($report_ids_array);
 
     $to = $email_addresses;
     $subject = 'Your RALF Impact Report';
-    $message = 'Your RALF Impact Report is attached to this email. Your chosen Activities is listed below:' . "\r\n\r\n";
+    //$headers = 'From: USAID RALF <jcampbell@childressagency.com>';
+    $headers = '';
+    $message = 'Your RALF Impact Report is attached to this email. Your chosen Activities are listed below:' . "\r\n\r\n";
 
     foreach($report_ids_array as $report_id){
       $message .= ' - ' . get_the_title($report_id) . "\r\n";
@@ -88,7 +94,8 @@ class ralf_report{
 
     $message .= "\r\n" . 'Here is a link back to your report: ' . esc_url(add_query_arg('report_ids', $report_ids, home_url('view-report')));
 
-    $result = wp_mail($to, $subject, $message);
+    //$result = wp_mail($to, $subject, $message, $headers, $rtf);
+    $result = wp_mail($to, $subject, $rtf, $headers);
 
     if($result == true){
       wp_send_json_success(__('Report email sent!', 'ralfreports'));
@@ -97,6 +104,44 @@ class ralf_report{
       wp_send_json_error();
     }
   }
+
+  function get_report($report_ids){
+    $rtf_report = '<h1>Report of Activities and Associated Impacts</h1>';
+
+    $activities_report = new WP_Query(array(
+      'post_type' => 'activities',
+      'posts_per_page' => -1,
+      'post__in' => $report_ids
+    ));
+    //$rtf_report = print_r($activities_report, true);
+
+    if($activities_report->have_posts()){
+      while($activities_report->have_posts()){
+        $activities_report->the_post();
+
+        $rtf_report .= '<h2>' . get_the_title() . '</h2>';
+        $rtf_report .= get_the_content();
+        $rtf_report .= '<h3>CONDITIONS</h3>';
+        $rtf_report .= get_field('conditions');
+
+        $impact_ids = get_field('related_impacts', false, false);
+        if(!empty($impact_ids)){
+          $impacts_by_sector = usaidralf_get_impacts_by_sector($impact_ids);
+          $rtf_report .= '<h3>IMPACT BY SECTOR</h3>';
+
+          foreach($impacts_by_sector as $sector){
+            foreach($sector['impacts'] as $impact){
+              $rtf_report .= '<h4>' . $impact->impact_title . '</h4>';
+              $rtf_report .= $impact->impact_description;
+            }
+          }
+        }
+
+      }
+    } wp_reset_postdata();
+
+    return $rtf_report;
+  }  
 }
 
 new ralf_report;
