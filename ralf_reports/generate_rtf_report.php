36426299a768af7f33737d986a2da6c6941372ab
childressdev <jcampbell@childressagency.com>
2018-09-12 11:26:04 -0400

    version 1

--- ralf_reports/generate_rtf_report.php
+++ ralf_reports/generate_rtf_report.php
@@ -15,7 +15,26 @@ function get_report($report_ids){
     while($activities_report->have_posts()){
       $activities_report->the_post();
 
-      
+      $rtf_report .= '<h2>' . get_the_title() . '</h2>';
+      $rtf_report .= get_the_content();
+      $rtf_report .= '<h3>CONDITIONS</h3>';
+      $rtf_report .= get_the_field('conditions');
+
+      $impact_ids = get_field('related_impacts', false, false);
+      if(!empty($impact_ids)){
+        $impacts_by_sector = usaidralf_get_impacts_by_sector($impact_ids);
+        $rtf_report .= '<h3>IMPACT BY SECTOR</h3>';
+
+        foreach($impacts_by_sector as $sector){
+          foreach($sector['impacts'] as $impact){
+            $rtf_report .= '<h4>' . $impact->impact_title . '</h4>';
+            $rtf_report .= $impact->impact_description;
+          }
+        }
+      }
+
     }
   } wp_reset_postdata();
+
+  return $rtf_report;
 }
