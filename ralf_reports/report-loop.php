f40224bd17b0b297f9af4aab2db79b054eae313d
childressdev <jcampbell@childressagency.com>
2018-10-01 16:22:51 -0400

    added article type icon

--- ralf_reports/report-loop.php
+++ ralf_reports/report-loop.php
@@ -1,6 +1,14 @@
 <article class="ralf-article">
   <header class="result-header">
-    <?php $report_article_id = get_the_ID(); ?>
+    <?php 
+      $report_article_id = get_the_ID(); 
+      if(get_post_type() == 'activities'){
+        echo '<span class="result-type-icon activity" data-toggle="tooltip" data-placement="top" title="Activity"></span>';
+      }
+      else if(get_post_type() == 'impacts'){
+        echo '<span class="result-type-icon impact" data-toggle="tooltip" data-placement="top" title="Impact"></span>';
+      }
+    ?>
     <h1 class="article-heading"><a href="#article_id-<?php echo $report_article_id ?>" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="article_id-<?php echo $report_article_id; ?>" class="collapsed"><?php the_title(); ?></a></h1>
   </header>
   <div id="article_id-<?php echo $report_article_id; ?>" class="collapse-container panel-collapse collapse" role="tab-panel" aria-labelledby="article-heading">
