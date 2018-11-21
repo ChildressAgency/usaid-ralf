016d8401e176e427d4b827649a149e10d760dc4a
childressdev <jcampbell@childressagency.com>
2018-11-21 11:07:13 -0500

    added custom pagination function

--- functions.php
+++ functions.php
@@ -286,6 +286,33 @@ function usaidralf_header_fallback_menu(){ ?>
 
 <?php }
 
+function usaidralf_pagination(){
+  global $wp_query;
+
+  if($wp_query->max_num_pages <= 1){ return; }
+
+  $big = 999999999;
+  $pages = paginate_links(array(
+            'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+            'format' => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total' => $wp_query->max_num_pages,
+            'type' => 'array'
+  ));
+
+  if(is_array($pages)){
+    $paged = (get_query_var('paged') == 0) ? 1 : get_query_var('paged');
+
+    echo '<nav aria-label="Page navigation"><ul class="pagination">';
+
+    foreach($pages as $page){
+      echo '<li>' . $page . '</li>';
+    }
+
+    echo '</ul></nav>';
+  }
+}
+
 add_action('init', 'usaidralf_create_post_type');
 function usaidralf_create_post_type(){
   $activity_labels = array(
