3bf5557233e9481dcbe162a7732b2f2052997848
childressdev <jcampbell@childressagency.com>
2018-11-29 12:01:53 -0500

    moved search history widget to proper class

--- sidebar.php
+++ sidebar.php
@@ -5,17 +5,4 @@
       <?php echo do_shortcode('[report_button]'); ?>
     </div>
   <?php endif; ?>
-  <?php 
-    $search_history = usaidralf_get_search_history();
-    if($search_history != ''){
-      $search_history_terms = explode(',', $search_history);
-
-      echo '<div class="sidebar-section"><h4>' . __('SEARCH HISTORY', 'usaidralf') . '</h4>';
-      echo '<a href="#" id="clear-search-history">[' . __('clear history', 'usaidralf') . ']</a><ul>';
-      foreach($search_history_terms as $search_term){
-        echo '<li><a href="' . esc_url(add_query_arg('s', $search_term, home_url())) . '">' . $search_term . '</a></li>';
-      }
-      echo '</ul></div>';
-    }
-  ?>
 </aside>
