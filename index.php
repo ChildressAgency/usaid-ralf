f81c4e596e95b2f1319aa3727d9a13eaa58ee77c
childressdev <jcampbell@childressagency.com>
2018-11-30 17:37:18 -0500

    added sidebar

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <div class="page-content">
   <div class="container">
     <div class="row">
+      <div class="col-sm-4 col-md-3">
+        <?php get_sidebar(); ?>
+      </div>
       <div class="col-sm-8 col-md-9">
         <main class="main">
           <?php if(have_posts()): while(have_posts()): the_post(); ?>
