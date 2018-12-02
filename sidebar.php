e21dc4ad9096a75374653c85bfe422593a7b3a37
John <jcampbell85.jc@gmail.com>
2018-12-01 22:06:39 -0500

    removed report button shortcode

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,3 @@
 <aside class="results-sidebar hidden-xs">
   <?php dynamic_sidebar('ralf-sidebar'); ?>
-  <?php if(is_singular('activities') || is_singular('impacts')): ?>
-    <div class="sidebar-section">
-      <?php echo do_shortcode('[report_button]'); ?>
-    </div>
-  <?php endif; ?>
 </aside>
