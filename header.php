e156845306820f353fb8f971747a894c4a9e0bff
John <jcampbell85.jc@gmail.com>
2018-12-03 07:28:30 -0500

    added print classes

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
       <div class="navbar-header">
         <a href="<?php echo home_url(); ?>" class="header-logo"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" class="img-responsive" alt="" /></a>
         <h1><?php _e('Environmental Compliance RALF', 'usaidralf'); ?></h1>
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <button type="button" class="navbar-toggle hidden-print collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
           <span class="sr-only">Toggle Navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
