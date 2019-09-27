a947ef8808e4dcca23ca3ca6288b01b21f43a151
childressdev <jcampbell@childressagency.com>
2019-09-27 15:14:30 -0400

    added get article meta

--- functions.php
+++ functions.php
@@ -211,6 +211,69 @@ function usaidralf_get_related_impacts($resource_id){
   return $impacts;
 }
 
+function usaidralf_get_article_meta($article_id){
+  $article_meta = array();
+
+  $article_type = get_post_type_object(get_post_type($article_id));
+  $article_meta['article_type'] = array(
+    'name' => $article_type->labels->singular_name,
+    'color' => get_field($article_type->name . '_color', 'option')
+  );
+
+  $sectors = get_the_terms($article_id, 'sectors');
+  if($sectors){
+    // sort sectors by parent
+    foreach($sectors as $key => $row){
+      $sector_parent[$key] = $row->parent;
+    }
+    array_multisort($sector_parent, SORT_ASC, $sectors);
+
+    $all_sectors = array();
+    foreach($sectors as $sector){
+      if($sector->parent > 0){
+        $all_sectors[] = $sector->parent;
+      }
+      $all_sectors[] = $sector->term_id;
+    }
+
+    $article_sectors = array_unique($all_sectors);
+    foreach($article_sectors as $article_sector){
+      $sector = get_term_by('id', $article_sector, 'sectors');
+      $article_meta['sectors'][] = array(
+        'name' => $sector->name,
+        'color' => get_field('sector_color', 'sectors_' . $sector->term_id),
+        'link' => get_term_link($sector->term_id, 'sectors')
+      );
+    }
+  }
+
+  if($article_type->name == 'impacts' || $article_type->name == 'resources'){
+    $related_activities = usaidralf_get_related_activities($article_id, $article_type->name);
+    $article_meta['related_activities_count'] = $related_activities->post_count;
+  }
+
+  if($article_type->name == 'resources'){
+    $related_impacts = usaidralf_get_related_impacts($article_id);
+    $article_meta['related_impacts_count'] = $related_impacts->post_count;
+
+    $resource_types = get_the_terms($article_id, 'resource_types');
+    foreach($resource_types as $key => $row){
+      $resource_types_parent[$key] = $row->parent;
+    }
+    array_multisort($resource_types_parent, SORT_ASC, $resource_types);
+
+    foreach($resource_types as $resource_type){
+      $article_meta['resource_types'][] = array(
+        'name' => $resource_type->name,
+        'color' => get_field('resource_type_color', 'resource_types_' . $resource_type->term_id),
+        'link' => get_term_link($resource_type->term_id, 'resource_types')
+      );
+    }
+  }
+
+  return $article_meta;
+}
+
 function usaidralf_show_article_meta($article_type, $article_id){
   switch($article_type){
     case 'impacts':
